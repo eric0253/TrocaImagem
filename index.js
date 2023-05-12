96eb74631353571989a99731d4c77d7fb51c5ed6
Eric Bochiccia <77254171+eric0253@users.noreply.github.com>
2023-05-12 07:01:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function on(){
+    document.getElementById("screen").src= "./images/content_screen.png";
+}
+
+function off(){
+    document.getElementById("screen").src= "./images/black_screen.png";
+}
+
+function idle(){
+    document.getElementById("screen").src= "./images/blank_screen.png";
+}
